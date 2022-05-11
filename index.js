43d29442b3076f38578df97f3e3812a62937d667
Cristian Triscu <cristian.triscu@bearingpoint.com>
2022-05-11 21:34:53 +0300

    mofificari de la seminar

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-
+const bodyParser = require("body-parser");
+const messagesRouter = require("../CC-SIMPRE/routers/messagesRouter.js");
 const app = express();
+
 app.use(cors());
-const port = 8080 || process.env;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/messages', messagesRouter);
+
+const port = process.env.port || 8080;
 
 app.get('/', (req, res) => {
     res.send("Hello Word!")
